09ad6fe530a4ea4708579f39ed8e2ed0888fee6f
Lionel <Lionel5garcia@gmail.com>
2022-07-19 13:07:00 +0200

    feat: nuevo controller y route para user update

--- contrellers/UserController.js
+++ contrellers/UserController.js
@@ -1,6 +1,35 @@
 const User = require("../models/User");
 const userController = {};
 
+userController.update = async(req, res) =>{
+    try{
+        const userId = req.user_id;
+
+        const update = {
+            name: req.body.name, 
+            surname: req.body.surname,
+            address: req.body.address,
+            city: req.body.city,
+            phone: req.body.phone,           
+        };
+                
+        const userUpdated = await User.findOneAndUpdate(userId, update, {new: true});
+        
+        return res.status(200).json({
+            success: true,
+            message: "User update success",
+            data: userUpdated
+        });    
+    }catch (error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error detected",
+            data: error?.message || error
+        })
+    }
+};
+
 userController.delete = async(req, res)=>{
     try{
         const id = req.user_id;
